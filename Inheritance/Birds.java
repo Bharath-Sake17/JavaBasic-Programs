77871345da6c35e23584c2bc8327d5203444d927
Bharath-Sake17 <sakebharath333@gmail.com>
2025-05-14 08:34:46 +0530

    Add files via upload

---
+++ Inheritance/Birds.java
@@ -0,0 +1,11 @@
+package polymorsphism.com;
+
+public class Birds extends Animal {
+	String bird;
+	
+	void birds()
+	{
+		System.out.println("Birds can fly");
+	}
+
+}
---
+++ Inheritance/Dogs.java
@@ -0,0 +1,11 @@
+package polymorsphism.com;
+
+public class Dogs extends Animal{
+	
+       String dogName;
+       
+	   void dog() {
+		   System.out.println("Dogs are braking");
+	   }
+
+}
